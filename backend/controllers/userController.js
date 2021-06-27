09d1d537ef3f3696ef243d4aabf86e1b3298a5b4
Mayank Kaushik <mayankkaushik187@gmail.com>
2021-06-27 20:44:15 +0530

    User login functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,3 +1,4 @@
+import { response } from "express"
 import asyncHandler from "express-async-handler"
 import User from "../models/userModel.js"
 import generateToken from "../utils/generateToken.js"
@@ -23,6 +24,38 @@ const authUser = asyncHandler(async (req, res) => {
     }
 })
 
+// @desc   Register a new User
+// @routes POST /api/users
+// @access PUBLIC
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body
+
+    const userExists = await User.findOne({ email })
+
+    if (userExists) {
+        res.status(400)
+        throw new Error("user already exists")
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password,
+    })
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id),
+        })
+    } else {
+        res.status(400)
+        throw new Error("Invalid user data")
+    }
+})
 // @desc   GET user profile
 // @routes POST /api/users/profile
 // @access Private
@@ -41,4 +74,4 @@ const getUserProfile = asyncHandler(async (req, res) => {
         throw new Error("user not found")
     }
 })
-export { authUser, getUserProfile }
+export { authUser, registerUser, getUserProfile }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,12 @@
 import express from "express"
 import { protect } from "../middleware/authMiddleWare.js"
-import { authUser, getUserProfile } from "../controllers/userController.js"
+import {
+    authUser,
+    getUserProfile,
+    registerUser,
+} from "../controllers/userController.js"
 const router = express.Router()
-
+router.route("/").post(registerUser)
 router.post("/login", authUser)
 router.route("/profile").get(protect, getUserProfile)
 
