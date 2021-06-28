9974b4803a825decb087fe8b1f1f12818f92abdf
Mayank Kaushik <mayankkaushik187@gmail.com>
2021-06-28 12:07:30 +0530

    User changes in Navbar

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { Link } from "react-router-dom"
 import { Form, Col, Row, Button } from "react-bootstrap"
 import { useDispatch, useSelector } from "react-redux"
@@ -6,18 +6,32 @@ import Message from "../components/Message"
 import Loader from "../components/Loader"
 import { login } from "../actions/userActions"
 import FormContainer from "../components/FormContainer"
-const LoginScreen = () => {
+
+const LoginScreen = ({ location, history }) => {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
 
-    const redirect = window.location.search
-        ? window.location.search.split("=")[1]
-        : "/"
+    const dispatch = useDispatch()
+    const userLogin = useSelector((state) => state.userLogin)
+    const { loading, error, userInfo } = userLogin
 
-    const submitHandler = () => {}
+    const redirect = location.search ? location.search.split("=")[1] : "/"
+
+    useEffect(() => {
+        if (userInfo) {
+            history.push(redirect)
+        }
+    }, [history, redirect, userInfo])
+    const submitHandler = (e) => {
+        e.preventDefault()
+        dispatch(login(email, password))
+        //DISPATCH LOGIN
+    }
     return (
         <FormContainer>
             <h1>Sign In</h1>
+            {error && <Message variant="danger">{error}</Message>}
+            {loading && <Loader />}
             <Form onSubmit={submitHandler}>
                 <Form.Group controlId="email">
                     <Form.Label>Email</Form.Label>
@@ -39,25 +53,24 @@ const LoginScreen = () => {
                     ></Form.Control>
                 </Form.Group>
 
-                <Button type="submit" variant="primary">
+                <Button className="signinbtn" type="submit" variant="primary">
                     Sign In
                 </Button>
-
-                <Row className="py-3">
-                    <Col>
-                        New Customer ?{" "}
-                        <Link
-                            to={
-                                redirect
-                                    ? `/register?redirect=${redirect}`
-                                    : "/register"
-                            }
-                        >
-                            Register
-                        </Link>
-                    </Col>
-                </Row>
             </Form>
+            <Row className="py-3">
+                <Col>
+                    New Customer ?{" "}
+                    <Link
+                        to={
+                            redirect
+                                ? `/register?redirect=${redirect}`
+                                : "/register"
+                        }
+                    >
+                        Register
+                    </Link>
+                </Col>
+            </Row>
         </FormContainer>
     )
 }
