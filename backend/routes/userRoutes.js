1cb31fe0df85a52e9be7934437c72a9961c816c4
Mayank Kaushik <mayankkaushik187@gmail.com>
2021-06-28 14:01:51 +0530

    PUT request for profile

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,10 +4,14 @@ import {
     authUser,
     getUserProfile,
     registerUser,
+    updateUserProfile,
 } from "../controllers/userController.js"
 const router = express.Router()
 router.route("/").post(registerUser)
 router.post("/login", authUser)
-router.route("/profile").get(protect, getUserProfile)
+router
+    .route("/profile")
+    .get(protect, getUserProfile)
+    .put(protect, updateUserProfile)
 
 export default router
