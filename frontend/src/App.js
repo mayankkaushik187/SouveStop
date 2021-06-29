ad707c995c9a3302d9a9e12d86c62681e2a94f39
Mayank Kaushik <mayankkaushik187@gmail.com>
2021-06-29 15:19:45 +0530

    Place Order Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,7 @@ import RegisterScreen from "./screens/RegisterScreen"
 import ProfileScreen from "./screens/ProfileScreen"
 import ShippingScreen from "./screens/ShippingScreen"
 import PaymentScreen from "./screens/PaymentScreen"
+import PlaceOrderScreen from "./screens/PlaceOrderScreen"
 
 const App = () => {
     return (
@@ -19,7 +20,7 @@ const App = () => {
             <main>
                 <Container>
                     <Route path="/login" component={LoginScreen} />
-
+                    <Route path="/placeorder" component={PlaceOrderScreen} />
                     <Route path="/register" component={RegisterScreen} />
                     <Route path="/profile" component={ProfileScreen} />
                     <Route path="/shipping" component={ShippingScreen} />
