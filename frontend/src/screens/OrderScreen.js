eb701a9374208531a836756414247e1a768d6ed5
Mayank Kaushik <mayankkaushik187@gmail.com>
2021-06-30 23:19:01 +0530

    OrderScreen useEffect

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -29,8 +29,10 @@ const OrderScreen = ({ match }) => {
     }
 
     useEffect(() => {
-        dispatch(getOrderDetails(orderId))
-    }, [])
+        if (!order || order._id !== orderId) {
+            dispatch(getOrderDetails(orderId))
+        }
+    }, [order, orderId])
 
     return loading ? (
         <Loader />
