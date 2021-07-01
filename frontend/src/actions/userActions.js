76d1e8c7a5b9c5ac3eae9b008cf154adacb80086
Mayank Kaushik <mayankkaushik187@gmail.com>
2021-07-01 14:53:06 +0530

    Bug Fix of My Orders

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -3,6 +3,7 @@ import {
     USER_DETAILS_FAIL,
     USER_DETAILS_REQUEST,
     USER_DETAILS_SUCCESS,
+    USER_DETAILS_RESET,
     USER_LOGIN_FAIL,
     USER_LOGIN_REQUEST,
     USER_LOGIN_SUCCESS,
@@ -14,7 +15,7 @@ import {
     USER_UPDATE_REQUEST,
     USER_UPDATE_SUCCESS,
 } from "../constants/userConstants"
-
+import { ORDER_LIST_MY_RESET } from "../constants/orderConstants"
 export const login = (email, password) => async (dispatch) => {
     try {
         dispatch({
@@ -53,7 +54,14 @@ export const login = (email, password) => async (dispatch) => {
 
 export const logout = () => (dispatch) => {
     localStorage.removeItem("userInfo")
+    localStorage.removeItem("cartItems")
+    localStorage.removeItem("shippingAddress")
+    localStorage.removeItem("paymentMethod")
     dispatch({ type: USER_LOGOUT })
+    dispatch({ type: USER_DETAILS_RESET })
+    dispatch({ type: ORDER_LIST_MY_RESET })
+    //dispatch({ type: USER_LIST_RESET })
+    document.location.href = "/login"
 }
 
 export const register = (name, email, password) => async (dispatch) => {
--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -1,14 +1,15 @@
 import {
-    USER_REGISTER_REQUEST,
     USER_LOGIN_REQUEST,
     USER_LOGIN_SUCCESS,
     USER_LOGIN_FAIL,
     USER_LOGOUT,
+    USER_REGISTER_REQUEST,
     USER_REGISTER_SUCCESS,
     USER_REGISTER_FAIL,
     USER_DETAILS_REQUEST,
     USER_DETAILS_SUCCESS,
     USER_DETAILS_FAIL,
+    USER_DETAILS_RESET,
     USER_UPDATE_REQUEST,
     USER_UPDATE_SUCCESS,
     USER_UPDATE_FAIL,
@@ -49,6 +50,10 @@ export const userDetailsReducer = (state = { user: {} }, action) => {
             return { loading: false, user: action.payload }
         case USER_DETAILS_FAIL:
             return { loading: false, error: action.payload }
+        case USER_DETAILS_RESET:
+            return {
+                user: {},
+            }
         default:
             return state
     }
