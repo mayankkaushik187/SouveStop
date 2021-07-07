ce48dc4717abf6464ea5e3e48e5b2b7e1b9f2f5d
Mayank Kaushik <mayankkaushik187@gmail.com>
2021-07-07 12:24:24 +0530

    Product List Screen

--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -1,40 +1,50 @@
 import React, { useEffect } from "react"
 import { LinkContainer } from "react-router-bootstrap"
-import { Table, Button } from "react-bootstrap"
+import { Table, Button, Row, Col } from "react-bootstrap"
 import { useDispatch, useSelector } from "react-redux"
 import Message from "../components/Message"
 import Loader from "../components/Loader"
-import { listUsers, deleteUser } from "../actions/userActions"
+import { listProducts } from "../actions/productActions"
 
-const UserListScreen = ({ history }) => {
+const ProductListScreen = ({ history, match }) => {
     const dispatch = useDispatch()
 
-    const userList = useSelector((state) => state.userList)
-    const { loading, error, users } = userList
+    const productList = useSelector((state) => state.productList)
+    const { loading, error, products } = productList
 
     const userLogin = useSelector((state) => state.userLogin)
     const { userInfo } = userLogin
 
-    const userDelete = useSelector((state) => state.userDelete)
-    const { success: successDelete } = userDelete
-
     useEffect(() => {
         if (userInfo && userInfo.isAdmin) {
-            dispatch(listUsers())
+            dispatch(listProducts())
         } else {
             history.push("/login")
         }
-    }, [dispatch, history, successDelete, userInfo])
+    }, [dispatch, history, userInfo])
 
     const deleteHandler = (id) => {
         if (window.confirm("Are you sure")) {
-            dispatch(deleteUser(id))
+            //Delete Products
         }
+    }
+    const createProductHandler = (product) => {
+        //CREATE PRODUCT
     }
 
     return (
         <>
-            <h1>Users</h1>
+            <Row>
+                <Col>
+                    <h1>Products</h1>
+                </Col>
+                <Col className="d-flex justify-content-end">
+                    <Button className="my-3" onCLick={createProductHandler}>
+                        <i className="fas fa-plus"></i> Create Product
+                    </Button>
+                </Col>
+            </Row>
+
             {loading ? (
                 <Loader />
             ) : error ? (
@@ -45,37 +55,23 @@ const UserListScreen = ({ history }) => {
                         <tr>
                             <th>ID</th>
                             <th>NAME</th>
-                            <th>EMAIL</th>
-                            <th>ADMIN</th>
+                            <th>PRICE</th>
+                            <th>CATEGORY</th>
+                            <th>BRAND</th>
                             <th></th>
                         </tr>
                     </thead>
                     <tbody>
-                        {users.map((user) => (
-                            <tr key={user._id}>
-                                <td>{user._id}</td>
-                                <td>{user.name}</td>
-                                <td>
-                                    <a href={`mailto:${user.email}`}>
-                                        {user.email}
-                                    </a>
-                                </td>
-                                <td>
-                                    {user.isAdmin ? (
-                                        <i
-                                            className="fas fa-check"
-                                            style={{ color: "green" }}
-                                        ></i>
-                                    ) : (
-                                        <i
-                                            className="fas fa-times"
-                                            style={{ color: "red" }}
-                                        ></i>
-                                    )}
-                                </td>
+                        {products.map((product) => (
+                            <tr key={product._id}>
+                                <td>{product._id}</td>
+                                <td>{product.name}</td>
+                                <td>${product.price}</td>
+                                <td>{product.category}</td>
+                                <td>{product.brand}</td>
                                 <td>
                                     <LinkContainer
-                                        to={`/admin/user/${user._id}/edit`}
+                                        to={`/admin/product/${product._id}/edit`}
                                     >
                                         <Button
                                             variant="light"
@@ -87,7 +83,9 @@ const UserListScreen = ({ history }) => {
                                     <Button
                                         variant="danger"
                                         className="btn-sm"
-                                        onClick={() => deleteHandler(user._id)}
+                                        onClick={() =>
+                                            deleteHandler(product._id)
+                                        }
                                     >
                                         <i className="fas fa-trash"></i>
                                     </Button>
@@ -101,4 +99,4 @@ const UserListScreen = ({ history }) => {
     )
 }
 
-export default UserListScreen
+export default ProductListScreen
