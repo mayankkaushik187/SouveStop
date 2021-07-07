c6d04a551180a38655966b9866a3d1b2283cc88f
Mayank Kaushik <mayankkaushik187@gmail.com>
2021-07-07 12:38:40 +0530

    Delete Product Functionality

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -21,4 +21,18 @@ const getProductById = asyncHandler(async (req, res) => {
     }
 })
 
-export { getProductById, getProducts }
+// @desc Delete single product
+// @routes DELETE /api/products/:id
+// @access PRivate/Admin
+const deleteProduct = asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
+    if (product) {
+        await product.remove()
+        res.json({ message: "Product Removed" })
+    } else {
+        res.status(404)
+        throw new Error("Product Not Found")
+    }
+})
+
+export { getProductById, getProducts, deleteProduct }
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,7 +2,9 @@ import express from "express"
 import {
     getProductById,
     getProducts,
+    deleteProduct,
 } from "../controllers/productController.js"
+import { protect, admin } from "../middleware/authMiddleWare.js"
 const router = express.Router()
 
 // @desc Fetch all products
@@ -13,6 +15,6 @@ router.route("/").get(getProducts)
 // @desc Fetch single product
 // @routes GET /api/products/:id
 // @access PUBLIC
-router.route("/:id").get(getProductById)
+router.route("/:id").get(getProductById).delete(protect, admin, deleteProduct)
 
 export default router
