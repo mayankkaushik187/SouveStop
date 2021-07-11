ebc6f71a2a1cb51ba9bc31640b66f8378893d082
Mayank Kaushik <mayankkaushik187@gmail.com>
2021-07-11 20:31:14 +0530

    Review for Products

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -15,6 +15,9 @@ import {
     PRODUCT_UPDATE_FAIL,
     PRODUCT_UPDATE_SUCCESS,
     PRODUCT_UPDATE_REQUEST,
+    PRODUCT_CREATE_REVIEW_FAIL,
+    PRODUCT_CREATE_REVIEW_SUCCESS,
+    PRODUCT_CREATE_REVIEW_REQUEST,
 } from "../constants/productConstants"
 export const listProducts = () => async (dispatch) => {
     try {
@@ -150,3 +153,38 @@ export const updateProduct = (product) => async (dispatch, getState) => {
         })
     }
 }
+
+export const createProductReview =
+    (productId, review) => async (dispatch, getState) => {
+        try {
+            dispatch({
+                type: PRODUCT_CREATE_REVIEW_REQUEST,
+            })
+
+            const {
+                userLogin: { userInfo },
+            } = getState()
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${userInfo.token}`,
+                },
+            }
+            const { data } = await axios.post(
+                `/api/products/${productId}`,
+                review,
+                config
+            )
+
+            dispatch({
+                type: PRODUCT_CREATE_REVIEW_SUCCESS,
+            })
+        } catch (error) {
+            dispatch({
+                type: PRODUCT_CREATE_REVIEW_FAIL,
+                payload:
+                    error.response && error.response.data.message
+                        ? error.response.data.message
+                        : error.message,
+            })
+        }
+    }
--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -2,6 +2,10 @@ import {
     PRODUCT_CREATE_FAIL,
     PRODUCT_CREATE_REQUEST,
     PRODUCT_CREATE_RESET,
+    PRODUCT_CREATE_REVIEW_FAIL,
+    PRODUCT_CREATE_REVIEW_REQUEST,
+    PRODUCT_CREATE_REVIEW_RESET,
+    PRODUCT_CREATE_REVIEW_SUCCESS,
     PRODUCT_CREATE_SUCCESS,
     PRODUCT_DELETE_FAIL,
     PRODUCT_DELETE_REQUEST,
@@ -62,13 +66,13 @@ export const productDeleteReducer = (state = {}, action) => {
 
 export const productCreateReducer = (state = {}, action) => {
     switch (action.type) {
-        case PRODUCT_CREATE_REQUEST:
+        case PRODUCT_CREATE_REVIEW_REQUEST:
             return { loading: true }
-        case PRODUCT_CREATE_SUCCESS:
-            return { loading: false, success: true, product: action.payload }
-        case PRODUCT_CREATE_FAIL:
+        case PRODUCT_CREATE_REVIEW_SUCCESS:
+            return { loading: false, success: true }
+        case PRODUCT_CREATE_REVIEW_FAIL:
             return { loading: false, error: action.payload }
-        case PRODUCT_CREATE_RESET:
+        case PRODUCT_CREATE_REVIEW_RESET:
             return {}
         default:
             return state
@@ -78,6 +82,21 @@ export const productCreateReducer = (state = {}, action) => {
 export const productUpdateReducer = (state = { product: {} }, action) => {
     switch (action.type) {
         case PRODUCT_UPDATE_REQUEST:
+            return { loading: true }
+        case PRODUCT_UPDATE_SUCCESS:
+            return { loading: false, success: true, product: action.payload }
+        case PRODUCT_UPDATE_FAIL:
+            return { loading: false, error: action.payload }
+        case PRODUCT_UPDATE_RESET:
+            return { product: {} }
+        default:
+            return state
+    }
+}
+
+export const productReviewCreateReducer = (state = { product: {} }, action) => {
+    switch (action.type) {
+        case PRODUCT_CREATE_REQUEST:
             return { loading: true }
         case PRODUCT_UPDATE_SUCCESS:
             return { loading: false, success: true, product: action.payload }
