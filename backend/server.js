0af0c0faef77c2d3020f6fb8b6d16d6772a094f9
Mayank Kaushik <mayankkaushik187@gmail.com>
2021-07-11 18:41:56 +0530

    Added morgan to log

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 import path from "path"
 import express from "express"
 import colors from "colors"
+import morgan from "morgan"
 import connectDB from "./config/db.js"
-
 import dotenv from "dotenv"
 
 import productRoutes from "./routes/productRoutes.js"
@@ -17,6 +17,9 @@ connectDB()
 
 const app = express()
 
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"))
+}
 app.use(express.json())
 
 app.get("/", (req, res) => {
