a97eca06c9206fd4563ddcb88feb49bb88bd7e06
Mayank Kaushik <mayankkaushik187@gmail.com>
2021-07-12 17:00:08 +0530

    Deployment fix

--- backend/server.js
+++ backend/server.js
@@ -53,7 +53,7 @@ app.use(errorHandler)
 const PORT = process.env.PORT || 5000
 
 app.listen(
-    5000,
+    PORT,
     console.log(
         `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
             .yellow.bold
